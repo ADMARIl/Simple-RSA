0243f5560ea0e66e15e5860e6a29eb1f97d04357
Andrew Ingson <aings1@umbc.edu>
2020-04-22 02:32:59 -0400

    Initial commit

---
+++ part1a.py
@@ -0,0 +1,17 @@
+"""
+File: part1a.py
+Created by Andrew Ingson (aings1@umbc.edu)
+Date: 4/22/2020
+CMSC 441 Intro to Algorithms
+
+"""
+
+
+def main():
+    print("#####   Part 1A   #####")
+    bits = int(input("Enter your desired modulus size: "))
+    print("Size of", bits, "bits selected.")
+
+
+if __name__ == "__main__":
+    main()
