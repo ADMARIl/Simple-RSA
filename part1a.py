5bdd7110be34f96ae444ed6851fc9fd9471fcce8
Andrew Ingson <aings1@umbc.edu>
2020-04-22 22:54:02 -0400

    added more comments to part1a.py

--- part1a.py
+++ part1a.py
@@ -14,11 +14,13 @@ COMPOSITE = False
 E_EXPO = 65537
 
 
+# extended euclidean from class notes
 def euclidean(a, b):
     # check for base case
     if b == 0:
         return a, 1, 0
     else:
+        # swap things around recursively
         d_p, x_p, y_p = euclidean(b, a % b)
         d, x, y = d_p, y_p, x_p - (a // b) * y_p
         return d, x, y
@@ -26,12 +28,12 @@ def euclidean(a, b):
 
 # Miller-Rabin as it is shown in the book (pg 970)
 def mr_test(n, s):
-    # print(n, s)
+    # check for primality s number of times
     for j in range(1, s):
         # random
         a = random.randint(1, n - 1)
         if witness(a, n):
-            # This is supposed to return COMPOSITE but idk what that's supposed to be
+            # Return this is a isn't prime
             return COMPOSITE
     return PRIME
 
@@ -44,12 +46,12 @@ def witness(a, n):
     while (sub_n & 1) != 0:
         sub_n = sub_n >> 1
         t += 1
-
+    # calculate U
     u = (n - 1) // (2 ** t)
     # print("T and U are be", t, u)
-
+    # modular exponentiation
     x = gmpy2.powmod(a, u, n)
-
+    # calculate prime likely hood
     previous = x
     current = x
     for i in range(t):
@@ -68,8 +70,10 @@ def getPrime(size, s):
     # Generate RSA primes
     curr_prime = random.randint((2 ** (size - 1)), (2 ** size) - 1)
     # print(prime1)
+    # check if its even
     if curr_prime & 1 == 0:
         curr_prime += 1
+    # keep incrementing by 2 till we reach a prime
     while mr_test(curr_prime, s) != PRIME:
         # print("incrementing prime by 2")
         curr_prime += 2
@@ -92,15 +96,11 @@ def getKeys(size):
     # calculate private key
     calc_gcd, ex, ny = euclidean(E_EXPO, (primeP-1)*(primeQ-1))
     d = ex
-    # d = int(d)
     print("---- BEGIN PRIVATE KEY ----")
     print("(", d, end="")
     print(", ", end='')
     print(n, ")")
     print("---- END PRIVATE KEY ----")
-#    print("\n\n")
-#    print(d*E_EXPO)
-#    print(1 % ((primeP-1) * (primeQ - 1)))
 
     return n, d
 
@@ -113,6 +113,7 @@ def main():
     # Generate RSA primes
     n, d = getKeys(bits)
 
+    # numberify message
     message = "I deserve an A"
     print("Before encrypt:", message)
     x = 0
@@ -120,17 +121,19 @@ def main():
         x = x << 8
         x = x ^ ord(c)
 
-    # message_num = 4
-    #  print(x)
+    # encrypt the message
     c = gmpy2.powmod(x, E_EXPO, n)
-    print("C:", c)
+    print("Encrypted signature:", c)
+    # decrypt the message
     m = gmpy2.powmod(c, d, n)
     # print("M:", m)
 
-    print(x)
-    print(m)
+    # print(x)
+    # print(m)
     if m == x:
         print("RSA Successful!")
+    else:
+        print("This shouldn't ever be reached")
 
 
 if __name__ == "__main__":
